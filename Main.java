f4eb6057572486b51f0002032c867d5ffb290686
Avika K <a.kapoor20@theabbey.co.uk>
2025-12-11 11:42:48 +0000

    Add Main class with main method and variables
    Initial creation of Main class with basic data types.

---
+++ Main.java
@@ -0,0 +1,24 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        int myNum = 5;
+        float myFloatNum = 5.99F;
+        double myDoubleNum = 19.999;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myDoubleNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+    }
+}
